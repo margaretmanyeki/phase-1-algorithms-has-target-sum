6fbed0c88acd38f9e7c8b82dc7fc31db49a927d4
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-17 21:48:38 +0300

    commit tree 8ce6bda4b2140e160b8c4ae438d5872d417b1789

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  for (const number of array) {
+    const complement = target - number;
+    if (seenNumbers[complement]) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
+
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
